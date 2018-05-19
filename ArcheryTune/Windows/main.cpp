842cd3327784c44e74f74411a08eba46e05ca9fa
Guy Barrand <guy.barrand@gmail.com>
2018-05-19 20:10:17 +0200

    update

---
+++ ArcheryTune/Windows/main.cpp
@@ -0,0 +1,13 @@
+// Copyright (C) 2010, Guy Barrand. All rights reserved.
+// See the file ArcheryTune.license for terms.
+
+#include "../ArcheryTune/main"
+
+#include <exlib/app/Windows/main_cpp>
+
+int main(int argc,char** argv) {return exlib_main<ArcheryTune::context,ArcheryTune::main>("ArcheryTune",argc,argv);}
+
+//exlib_build_use inlib expat
+//exlib_build_use exlib png jpeg zlib inlib_glutess freetype
+//exlib_build_use GL Windows kernel
+
